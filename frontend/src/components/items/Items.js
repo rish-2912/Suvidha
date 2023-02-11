778e79d1cdd1773c5eed5874b075203d12609662
rish-2912 <jainrishabh0607@gmail.com>
2023-02-12 04:15:55 +0530

    carousel data

--- frontend/src/components/items/Items.js
+++ frontend/src/components/items/Items.js
@@ -8,26 +8,33 @@ import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import Search from "./Search";
 import Carousel from 'react-material-ui-carousel'
 import { Paper } from '@mui/material'
+import rajkot from "../../assets/rajkot.png"
+import tran from "../../assets/tran.png"
+import joshimath from "../../assets/joshimath.png"
 const arr = [
     {
         "id": "1",
         "title": "A Safe Home To Thousands Of Cows, This Gaushala Now Needs Your Help To Give Them A Better Life",
-        "story": "80 children have found a home and a loving family in Manalo Manam started by a well-meaning couple in Ongole. Donate groceries and construction materials to support their mission."
+        "story": "80 children have found a home and a loving family in Manalo Manam started by a well-meaning couple in Ongole. Donate groceries and construction materials to support their mission.",
+        "img": `${demo}`
     },
     {
         "id": "2",
-        "title": "Parents To 80 Orphaned Kids, This Couple Is Working Tirelessly To Give Them A Better Life",
-        "story": "80 children have found a home and a loving family in Manalo Manam started by a well-meaning couple in Ongole. Donate groceries and construction materials to support their mission."
+        "title": "Vijay Is Working Tirelessly For The Abandoned Elderly, Join Him To Support His Cause",
+        "story": "Home to 400+ elderly, this NGO in Rajkot is working tirelessly to give them a happy and comfortable life. You can join their mission by donating groceries and hygiene products.",
+        'img': `${rajkot}`
     },
     {
         "id": "3",
-        "title": "A Safe Home To Thousands Of Cows, This Gaushala Now Needs Your Help To Give Them A Better Life",
-        "story": "80 children have found a home and a loving family in Manalo Manam started by a well-meaning couple in Ongole. Donate groceries and construction materials to support their mission."
+        "title": "Despite Battling Cancer, This Trans Woman Is Working Tirelessly To Support The Elderly",
+        "story": "Even as she battles blood cancer, Nakshatra keeps worrying about the elderlies at her shelter home. She is determined to give them a better life, and you can help by donating groceries and construction materials.",
+        "img": `${tran}`
     },
     {
         "id": "4",
-        "title": "A Safe Home To Thousands Of Cows, This Gaushala Now Needs Your Help To Give Them A Better Life",
-        "story": "80 children have found a home and a loving family in Manalo Manam started by a well-meaning couple in Ongole. Donate groceries and construction materials to support their mission."
+        "title": "Hundreds Of People & Animals Have Been Devastated By The Joshimath Disaster, Donate To Save Them",
+        "story": "Hundreds of people have been forced to leave their homes as Joshimath in Uttarakhand has been ‘sinking’. Countless strays are at risk of losing their lives too. Your support will make a huge difference",
+        "img": `${joshimath}`
     },
 ]
 const Itembox = styled(Box)(({ theme }) => ({
@@ -52,7 +59,7 @@ export default class Items extends Component {
                                 return (
                                     <Itembox key={item.id} style={{ padding: '16px', fontFamily: 'Poppins' }}>
                                         <Box style={{ display: 'flex', padding: '16px' }}>
-                                            <img src={demo} style={{ height: '200px', width: '30%' }} />
+                                            <img src={item.img} style={{ height: '200px', width: '30%' }} />
                                             <Box style={{ width: '70%', textAlign: 'left', padding: '0 24px' }}>
                                                 <Typography style={{ background: 'red', width: '100px', textAlign: 'center', borderRadius: '12px', color: 'white', fontFamily: 'inherit', fontWeight: 'bold', margin: '7px 0' }}>Featured</Typography>
                                                 <Typography style={{ fontFamily: 'inherit', fontWeight: '600', paddingTop: '6px' }}>{item.title}</Typography>
